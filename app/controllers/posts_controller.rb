eb6aaace98a91d5c990b4b29b77624b84d289881
Joshua Jarvis <bynarlogic@gmail.com>
2020-05-12 18:04:23 -0400

    Toy app demonstrating Stimulus Reflex

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class PostsController < ApplicationController
+  include CableReady::Broadcaster
+
+  def index
+    @posts = Post.all.order(created_at: :desc)
+    @post = Post.new
+  end
+
+  def create
+    post = Post.create(post_params)
+    cable_ready['timeline'].insert_adjacent_html(
+      selector: '#timeline',
+      position: 'afterbegin',
+      html: render_to_string(partial: 'post', locals: { post: post })
+    )
+    cable_ready.broadcast
+    redirect_to posts_path
+  end
+
+  private
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def post_params
+      params.require(:post).permit(:body)
+    end
+end
